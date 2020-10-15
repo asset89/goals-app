d696bb92e1e15de8abe0d452d9c978dde988db88
Asset Ryskul <asset89@gmail.com>
2020-10-15 14:13:44 +0600

    goals-app started

---
+++ goals-app/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goals-app
+//
+//  Created by Asset Ryskul on 10/15/20.
+//  Copyright © 2020 Asset Ryskul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
